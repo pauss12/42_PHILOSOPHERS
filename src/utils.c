f56e834bddec05bdccc8ab83fd3c947eb6dde7c7
pmendez- <paulamendezsv@gmail.com>
2025-05-17 22:46:33 +0200

    creacion funcion para mostrar mensaje

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: paula <paulamendezsv@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 13:34:57 by paula             #+#    #+#             */
-/*   Updated: 2025/05/17 18:25:23 by paula            ###   ########.fr       */
+/*   Updated: 2025/05/17 21:44:31 by paula            ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -27,4 +27,11 @@ void	print_error(char *error_message)
 	}
 	ft_putendl_fd(str, 2);
 	free(str);
+}
+
+void print_message(t_philo *philo, char *message)
+{
+	pthread_mutex_lock(&((t_data *)(philo->data))->print);
+	ft_printf("%s%d %s%s\n", ORANGE, philo->id_philo, message, RESET);
+	pthread_mutex_unlock(&((t_data *)(philo->data))->print);
 }
