acd6d9c95e84dc3f576d91225038694f20400666
pmendez- <paulamendezsv@gmail.com>
2025-05-17 18:33:16 +0200

    he quitado la creacion del hilo monitor

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: paula <paulamendezsv@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 13:34:57 by paula             #+#    #+#             */
-/*   Updated: 2025/05/12 21:03:52 by paula            ###   ########.fr       */
+/*   Updated: 2025/05/17 18:25:23 by paula            ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -16,21 +16,15 @@ void	print_error(char *error_message)
 {
 	char	*str;
 
-	if (cmd == NULL)
+	if (error_message == NULL)
 		return ;
 	str = ft_strjoin_variadica(3, RED "Error\n" RESET, ": ", error_message);
 	if (str == NULL)
 	{
-		ft_printf(RED "Error\n" END "Memory allocation failed");
+		ft_printf(RED "Error\n" RESET);
+		ft_printf("Memory allocation failed");
 		return ;
 	}
 	ft_putendl_fd(str, 2);
 	free(str);
-}
-
-void	print_message(t_data *data, int id_philo, char *message)
-{
-	pthread_mutex_lock(data->print);
-	
-	pthread_mutex_unlock(data->print);
 }
