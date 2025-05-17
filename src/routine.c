c0bc066f457b4224b294b21d0f1cc4c8e17183a3
pmendez- <paulamendezsv@gmail.com>
2025-05-17 22:45:46 +0200

    sincronizacion

--- src/routine.c
+++ src/routine.c
@@ -6,38 +6,58 @@
 /*   By: paula <paulamendezsv@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 13:16:09 by paula             #+#    #+#             */
-/*   Updated: 2025/05/12 21:12:59 by paula            ###   ########.fr       */
+/*   Updated: 2025/05/17 22:45:38 by paula            ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "philo.h"
 
-int	sleeping(t_data *data)
-{
-	long start_time;
+// int	sleeping(t_data *data)
+// {
+// 	long start_time;
 
-	start_time = get_time();
-	while (get_time() - start_time < data->philo->time_to_sleep)
+// 	start_time = get_time();
+// 	while (get_time() - start_time < data->philo->time_to_sleep)
+// 	{
+// 		if (data->philo->is_dead == 1)
+// 			return (NULL);
+// 		usleep(100);
+// 	}
+// }
+
+void *routine(void *arg)
+{
+	t_data		*data;
+	t_philo		*philo;
+
+	philo = (t_philo *)arg;
+	data = (t_data *)philo->data;
+
+	//EL hilo se queda bloqueado aqui
+	pthread_mutex_lock(&data->init);
+	pthread_mutex_unlock(&data->init);
+	
+	if (philo->id_philo == 1)
 	{
-		if (data->philo->is_dead == 1)
-			return (NULL);
-		usleep(100);
+		print_message(philo, "A -");
+	}
+	else if (philo->id_philo == 2)
+	{
+		usleep(1000);
+		print_message(philo, "B -");
+	}
+	else if (philo->id_philo == 3)
+	{
+		usleep(2000);
+		print_message(philo, "C -");
 	}
 
+	return (NULL);
 }
 
-void routine(t_data *data)
-{
-	//Esta muerto¿?
-	if (data->is_dead == 1)
-		return ;
 
-	//Comer
-	is_eating(data);
 
-	//Dormir
-	sleeping(data);
-
-	//Pensar
-	thinking(data);
-}
+// printf("El valor es: %d\n", data->valor);
+// data->valor++;
+//printf("im here\n");
+// printf("El valor DESPUES : %d\n", data->valor);
