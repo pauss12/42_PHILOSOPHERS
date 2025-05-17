f1fd0042edd6167b31bff50d307a996b41f1ca0e
pmendez- <paulamendezsv@gmail.com>
2025-05-17 22:46:58 +0200

    philo.h

--- src/philo.h
+++ src/philo.h
@@ -6,7 +6,7 @@
 /*   By: paula <paulamendezsv@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 20:04:41 by pmendez-          #+#    #+#             */
-/*   Updated: 2025/05/12 20:50:00 by paula            ###   ########.fr       */
+/*   Updated: 2025/05/17 22:02:14 by paula            ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -32,13 +32,13 @@ typedef struct s_philo
 {
 	int				id_philo;
 	int				times_eaten;
-	int				is_dead;
 	int				time_to_die;
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				times_each_philosopher_must_eat;
 	pthread_mutex_t	*fork_right;
 	pthread_mutex_t	*fork_left;
+	struct t_data	*data;
 	pthread_t		thread;
 
 }	t_philo;
@@ -46,12 +46,14 @@ typedef struct s_philo
 typedef struct s_data
 {
 	int				num_philos;
+	int				valor;
 	t_philo			*philo;
 	pthread_mutex_t	*forks;
-	pthread_mutex_t	*print;
-	pthread_mutex_t	*dead;
-	pthread_mutex_t	*eat;
-	int				loop_finished;
+	pthread_mutex_t	print;
+	pthread_mutex_t	init;
+	// pthread_mutex_t	*dead;
+	//pthread_mutex_t	eat;
+	int				is_dead;
 
 }	t_data;
 
@@ -64,19 +66,20 @@ int		check_args(int argc, char **argv);
 // ############################# UTILS.C #######################################
 // #############################################################################
 void	print_error(char *error_message);
+void	print_message(t_philo *philo, char *message);
 
 // #############################################################################
 // ############################# TREAT_MEMORY ##################################
 // #############################################################################
-// void	initialize_struct(t_data *data);
-// void	initialize_philos(t_data *data, char *argv[]);
+void	initialize_struct(t_data *data, char *argv[]);
+void	initialize_philos(t_data *data, char *argv[]);
 void	free_struct(t_data *data);
 
 // #############################################################################
 // ############################# RUTINAS #######################################
 // #############################################################################
-void	*sleeping(t_data *data);
-void	*routine(t_data *data);
+//void	*sleeping(t_data *data);
+void	*routine(void *arg);
 
 // ##############################################################################
 // ############################# CREATE_THREADS ################################
