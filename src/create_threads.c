db73a1a3e3bb426036a4eb2bf7428ad2d963b1a4
pmendez- <paulamendezsv@gmail.com>
2025-05-17 22:44:59 +0200

    sincronizacion

--- src/create_threads.c
+++ src/create_threads.c
@@ -6,7 +6,7 @@
 /*   By: paula <paulamendezsv@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 20:37:26 by paula             #+#    #+#             */
-/*   Updated: 2025/05/17 15:53:39 by paula            ###   ########.fr       */
+/*   Updated: 2025/05/17 22:44:46 by paula            ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -21,26 +21,40 @@ static void wait_for_threads(t_data *data)
 	{
 		if (pthread_join(data->philo[i].thread, NULL) != 0)
 		{
+			pthread_mutex_lock(&data->print);
 			print_error("Error joining philosopher thread");
-			free_struct(data);
+			pthread_mutex_unlock(&data->print);
+			//free_struct(data);
 		}
 		i++;
 	}
+	ft_printf("All threads joined successfully.\n");
 }
+
+
 
 void create_threads(t_data *data)
 {
 	int			i;
 
 	i = 0;
+	ft_printf("Creating threads...\n");
+	pthread_mutex_lock(&data->init);
 	while (i < data->num_philos)
 	{
+		data->philo[i].data = (struct t_data *)data;
 		if (pthread_create(&data->philo[i].thread, NULL, routine, &data->philo[i]) != 0)
 		{
 			print_error("Error creating philosopher thread");
-			free_struct(data);
 		}
 		i++;
 	}
+	printf("Threads created successfully.\n");
+	pthread_mutex_unlock(&data->init);
+
+	//Comprueba si alguien ha muerto
+	check_status(data);
+
+	// Wait for all threads to finish
 	wait_for_threads(data);
 }
