aeabbf37c22e774384ac89637bff35eeeb5a8bb9
Paula <paulamendezsv@gmail.com>
2025-07-22 18:47:37 +0200

    cambio funcion de tenedores

--- src/forkTreatment.c
+++ src/forkTreatment.c
@@ -6,33 +6,15 @@
 /*   By: pmendez- <pmendez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 21:27:34 by pmendez-          #+#    #+#             */
-/*   Updated: 2025/07/21 20:07:51 by pmendez-         ###   ########.fr       */
+/*   Updated: 2025/07/22 18:39:59 by pmendez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// void takeForks(t_philo *philo)
-// {
-//     if (philo->id_philo % 2 != 0 && philo->nb_philos % 2 == 0)
-//     {
-//         pthread_mutex_lock(philo->fork_right);
-//         print_message_philo(philo, TAKEN_RIGHT_FORK);  
-//         pthread_mutex_lock(philo->fork_left);
-//         print_message_philo(philo, TAKEN_LEFT_FORK);
-//     }
-//     else
-//     {
-//         pthread_mutex_lock(philo->fork_left);
-//         print_message_philo(philo, TAKEN_LEFT_FORK);
-//         pthread_mutex_lock(philo->fork_right);
-//         print_message_philo(philo, TAKEN_RIGHT_FORK);
-//     }
-// }
-
 void takeForks(t_philo *philo)
 {
-    if (philo->fork_left < philo->fork_right)
+    if (philo->id_philo % 2 != 0 && philo->nb_philos % 2 == 0)
     {
         pthread_mutex_lock(philo->fork_right);
         print_message_philo(philo, TAKEN_RIGHT_FORK);  
@@ -47,6 +29,24 @@ void takeForks(t_philo *philo)
         print_message_philo(philo, TAKEN_RIGHT_FORK);
     }
 }
+
+// void takeForks(t_philo *philo)
+// {
+//     if (philo->id_philo % 2 != 0)
+//     {
+//         pthread_mutex_lock(philo->fork_right);
+//         print_message_philo(philo, TAKEN_RIGHT_FORK);  
+//         pthread_mutex_lock(philo->fork_left);
+//         print_message_philo(philo, TAKEN_LEFT_FORK);
+//     }
+//     else
+//     {
+//         pthread_mutex_lock(philo->fork_left);
+//         print_message_philo(philo, TAKEN_LEFT_FORK);
+//         pthread_mutex_lock(philo->fork_right);
+//         print_message_philo(philo, TAKEN_RIGHT_FORK);
+//     }
+// }
 
 // static void print_message_taking_forks(t_philo *philo, int right_fork_first, int first_try)
 // {
@@ -107,3 +107,19 @@ void releaseForks(t_philo *philo)
         print_message_philo(philo, RELEASE_FORKS);
     }
 }
+
+// void releaseForks(t_philo *philo)
+// {
+//     if (philo->id_philo % 2 != 0)
+//     {
+//         pthread_mutex_unlock(philo->fork_left);
+//         pthread_mutex_unlock(philo->fork_right);
+//         print_message_philo(philo, RELEASE_FORKS);
+//     }
+//     else
+//     {
+//         pthread_mutex_unlock(philo->fork_right);
+//         pthread_mutex_unlock(philo->fork_left);
+//         print_message_philo(philo, RELEASE_FORKS);
+//     }
+// }
